7c4d410b9673ad5c7f15504aa713919e25462a1d
lromang <luis.roangarci@gmail.com>
2016-02-29 13:55:00 -0600

    inst

--- cache.c
+++ cache.c
@@ -85,40 +85,95 @@ void init_cache()
 {
   /* initialize the cache, and cache statistics data structures */
 
-  /*------------------------------------------*/
+
   /* Inicializar tamaño */
-  /*------------------------------------------*/
+  /* Split == FALSE*/
   if(cache_split == 0){
     ucache = dcache = icache  = &c1;
     ucache->size          = cache_usize;
     ucache->associativity = cache_assoc;
     ucache->n_sets        = (cache_usize * cache_assoc) / cache_block_size;
-    /*------------------------------------------*/
+
     /* Mascaras */
-    /*------------------------------------------*/
     ucache->index_mask_offset = LOG2(cache_block_size);
     ucache->index_mask        = ~(0xFFFFFFFF << (ucache->index_mask_offset +
                                                  LOG2(ucache->n_sets)));
-    /*------------------------------------------*/
+
     /* Allocación de memoria */
-    /*------------------------------------------*/
-    ucache->LRU_head     = (Pcache_line *)malloc(sizeof(Pcache_line) * ucache->n_sets);    
+    ucache->LRU_head     = (Pcache_line *)malloc(sizeof(Pcache_line) * ucache->n_sets);
     ucache->LRU_tail     = (Pcache_line *)malloc(sizeof(Pcache_line) * ucache->n_sets);
     ucache->set_contents = (int *)malloc(sizeof(int) * ucache->n_sets);
-    /*------------------------------------------*/
+
     /* Vaciar contenido cache */
-    /*------------------------------------------*/
     for (int i = 0; i < ucache->n_sets; i++)
       {
         ucache->LRU_head[i] = NULL;
-        ucache->LRU_tail[i] = NULL;    
+        ucache->LRU_tail[i] = NULL;
         ucache->set_contents[i]= 0;
       }
     ucache->contents = 0;
     free_cache_lines = NULL;
+    /*------------------------------------------------------------------------*/
+    /* Split == TRUE*/
+    /*------------------------------------------------------------------------*/
   }else{
     icache = &c1;
     dcache = &c2;
+
+    /* Inicializar tamaño */
+
+    /* instrucciones */
+    icache->size          = cache_isize;
+    icache->associativity = cache_assoc;
+    icache->n_sets        = (cache_isize * cache_assoc) / cache_block_size;
+
+    /* datos */
+    dcache->size          = cache_dsize;
+    dcache->associativity = cache_assoc;
+    dcache->n_sets        = (cache_dsize * cache_assoc) / cache_block_size;
+
+    /* Mascaras */
+    /* instrucciones */
+    icache->index_mask_offset = LOG2(cache_block_size);
+    icache->index_mask        = ~(0xFFFFFFFF << (icache->index_mask_offset +
+                                                 LOG2(icache->n_sets)));
+    /* datos */
+    dcache->index_mask_offset = LOG2(cache_block_size);
+    dcache->index_mask        = ~(0xFFFFFFFF << (dcache->index_mask_offset +
+                                                 LOG2(dcache->n_sets)));
+
+    /* Allocación de memoria */
+    /* instrucciones */
+    icache->LRU_head     = (Pcache_line *)malloc(sizeof(Pcache_line) * icache->n_sets);
+    icache->LRU_tail     = (Pcache_line *)malloc(sizeof(Pcache_line) * icache->n_sets);
+    icache->set_contents = (int *)malloc(sizeof(int) * icache->n_sets);
+
+    /* datos */
+    dcache->LRU_head     = (Pcache_line *)malloc(sizeof(Pcache_line) * dcache->n_sets);
+    dcache->LRU_tail     = (Pcache_line *)malloc(sizeof(Pcache_line) * dcache->n_sets);
+    dcache->set_contents = (int *)malloc(sizeof(int) * dcache->n_sets);
+
+    /* Vaciar contenido cache */
+    /* instrucciones */
+    for (int i = 0; i < icache->n_sets; i++)
+      {
+        icache->LRU_head[i] = NULL;
+        icache->LRU_tail[i] = NULL;
+        icache->set_contents[i]= 0;
+      }
+    icache->contents = 0;
+
+    /* instrucciones */
+    for (int i = 0; i < dcache->n_sets; i++)
+      {
+        dcache->LRU_head[i] = NULL;
+        dcache->LRU_tail[i] = NULL;
+        dcache->set_contents[i]= 0;
+      }
+    dcache->contents = 0;
+
+    /* No hay líneas vacías*/
+    free_cache_lines = NULL;
   }
   /*------------------------------------------*/
   /* Vaciar contenido stats */
@@ -143,15 +198,12 @@ Pcache_line get_free_line(void)
     {
     Pcache_line head = free_cache_lines;
     
-    if (head != NULL)
-        {
+    if(head != NULL){
         free_cache_lines = head->LRU_next;
-        }
-    else
-        {
+        }else{
         head = (Pcache_line)malloc(sizeof(cache_line));
 
-        if (head == NULL)
+        if(head == NULL)
             return NULL;
         }
     
@@ -164,33 +216,28 @@ Pcache_line get_free_line(void)
 
 void insert_head(Pcache_line * head, Pcache_line * tail, Pcache_line item)
     {
-    if (item->LRU_prev)
-        {
+    if(item->LRU_prev){
         item->LRU_prev->LRU_next = item->LRU_next;
-        }
-    else
-        {
-        /* item at head */
+        }else{
+        /* item en cabeza */
         return;
         }
 
-    if (item->LRU_next)
-        {
+    if(item->LRU_next){
         item->LRU_next->LRU_prev = item->LRU_prev;
-        }
-    else
-        {
-        /* item at tail */
+        }else{
+        /* item en cola */
         *tail = item->LRU_prev;
-        }    
-    
+        }
+
     item->LRU_next = *head;
     item->LRU_prev = (Pcache_line)NULL;
 
-    if (item->LRU_next)
+    if(item->LRU_next){
         item->LRU_next->LRU_prev = item;
-    else
+    }else{
         *tail = item;
+    }
 
     *head = item;
     }
@@ -251,7 +298,7 @@ void perform_access(addr, access_type)
           insert_head(&ucache->LRU_head[index],
                  &ucache->LRU_tail[index],
                  LRU_aux);
-          
+
           /* Sale de rutina */
           return;
         }
@@ -325,8 +372,82 @@ void perform_access(addr, access_type)
              index, ucache->n_sets);
       return;
     }
+    /*------------------------------------------------------------------------*/
+    /* Split == TRUE*/
+    /*------------------------------------------------------------------------*/
   }else{
 
+    /* Instrucciones */
+    if(access_type == TRACE_INST_LOAD){ 
+
+      /* Calcula índice y tag */
+      index = (addr & icache->index_mask)  >> icache->index_mask_offset;
+      tag   = (addr & ~icache->index_mask) >> (icache->index_mask_offset +
+                                             LOG2(icache->n_sets));
+      
+      /* Verifica si el índice está en el rango de direcciones */
+      if((int)index <= icache->n_sets){
+
+        /* Accede a la línea de interés  e incrementa número de accesos */
+        LRU_aux = icache->LRU_head[index];
+
+        /*
+          Recorrer todos los ways de la línea
+          en búsqueda del tag.
+        */
+        while(LRU_aux != NULL){
+
+          /* Verificar si los tags coinciden*/
+          if(LRU_aux->tag == tag){
+
+            /* Si encuentra, sale de rutina */
+            return;
+          }
+
+          /* Obtiene siguiente elemento en la línea*/
+          LRU_aux = LRU_aux->LRU_next;
+        }
+
+        /* En caso de no obtener un match*/
+        cache_stat_inst.misses++;
+        cache_stat_inst.demand_fetches += words_per_block;
+        
+        /*
+          En caso de fallo, verifica si la línea está llena y
+          realiza reemplazo LRU (en la cola)
+        */
+        if(icache->set_contents[index] == icache->associativity){
+
+          /* Reemplazar tag de la cola */
+          icache->LRU_tail[index]->tag = tag;
+
+          /* Incrementa número de reemplazos */
+          cache_stat_inst.replacements++;
+
+          return;
+        }
+
+        /*
+          En caso de fallo, si la línea no está
+          llena obtiene un way y realiza una inserción.
+        */
+        LRU_aux = get_free_line();
+        
+        if(LRU_aux != NULL){
+          insert(&icache->LRU_head[index],
+                 &icache->LRU_tail[index],
+                 LRU_aux);
+          
+          icache->set_contents[index]++;
+          LRU_aux->tag = tag;
+        }else{
+          exit(-1);
+        }
+      }
+      /* Datos */
+      }else{
+
+    }
   }
 }
 /************************************************************/
@@ -338,26 +459,33 @@ void flush()
 
     /* flush the cache */
 
-    if (cache_split == FALSE) /* Unified cache */
-        {
-        for (int i = 0; i < ucache->n_sets; i++)
-            {
-            LRU_aux = ucache->LRU_head[i];
-
-            while (LRU_aux != NULL)
-                {
-                if (LRU_aux->dirty == TRUE)
-                    {
-                    cache_stat_data.copies_back += words_per_block;
-                    LRU_aux->dirty = FALSE;
-                    }
-                LRU_aux = LRU_aux->LRU_next;
-                }
-            }
+    /* Split == FALSE*/
+    if(cache_split == FALSE) {
+      for(int i = 0; i < ucache->n_sets; i++){
+        LRU_aux = ucache->LRU_head[i];
+        while(LRU_aux != NULL){
+          if(LRU_aux->dirty == TRUE){
+            cache_stat_data.copies_back += words_per_block;
+            LRU_aux->dirty = FALSE;
+          }
+          LRU_aux = LRU_aux->LRU_next;
         }
-    else                      /* Split cache */
-        {
+      }
+      /*------------------------------------------------------------------------*/
+      /* Split == TRUE*/
+      /*------------------------------------------------------------------------*/
+    }else{
+      for(int i = 0; i < dcache->n_sets; i++){
+        LRU_aux = dcache->LRU_head[i];
+        while(LRU_aux != NULL){
+          if(LRU_aux->dirty == TRUE){
+            cache_stat_data.copies_back += words_per_block;
+            LRU_aux->dirty = FALSE;
+          }
+          LRU_aux = LRU_aux->LRU_next;
         }
+      }
+    }
 }
 /************************************************************/
 
