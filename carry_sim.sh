cea3b9426a1e27b904721ae71cd2c54e2f767404
lromang <luis.roangarci@gmail.com>
2016-03-07 11:24:52 -0600

    run tests

--- carry_sim.sh
+++ carry_sim.sh
@@ -1,35 +1,106 @@
 #! /bin/bash
 
-# Starting conditions
-cache_size=4
+## ---------------------------
+## Impact of Cache Size
+## ---------------------------
+
+## Starting conditions
+cache_size=8
 hit_rate_prev=0
 assoc=$(($cache_size/4))
+block_size=4
 
-# First evaluation
-sim=$(./sim -bs 16 -a $assoc -wb -wa -ds $cache_size -is $cache_size  ./trazas/spice.trace | grep hit | awk -F '(' '{print $2}' | sed -r 's/[^0-9.]//g')
+## name of trace file
+trace=$1
+var=$(echo $1 | awk -F '.' '{print $1}')
+file="output/cache_size_$var.csv"
+
+## First evaluation
+
+sim=$(./sim -bs $block_size -a $assoc -wb -wa -ds $cache_size -is $cache_size  "./trazas/$trace" |
+             grep hit | awk -F '(' '{print $2}' | sed -r 's/[^0-9.]//g')
 row=$(echo $sim | sed 's/ /,/g')
-echo "cache_size, block_size, associativity, inst_hit, data_hit" > block_size.csv
-echo "$cache_size,4,$assoc,$row" >> block_size.csv
+echo "cache_size, block_size, associativity, inst_hit, data_hit" > $file
+echo "$cache_size,$block_size,$assoc,$row" >> $file
 
-# While
+## While
 flag=$(echo $hit_rate_prev '==' $(echo $sim | cut -d ' ' -f1) | bc -l)
 
 while [ $flag -eq 0 ]
 do
-    # Update hit_rate, cache_size, assoc, sim and row
+    ## Update hit_rate, cache_size, assoc, sim and row
     hit_rate_prev=$(echo $sim | cut -d ' ' -f1)
     cache_size=$(($cache_size*2))
     assoc=$(($cache_size/4))
-    sim=$(./sim -bs 4 -a $assoc -wb -wa -ds $cache_size -is $cache_size  ./trazas/spice.trace |
+    sim=$(./sim -bs $block_size -a $assoc -wb -wa -ds $cache_size -is $cache_size  "./trazas/$trace" |
                  grep hit | awk -F '(' '{print $2}' | sed -r 's/[^0-9.]//g')
     row=$(echo $sim | sed 's/ /,/g')
 
-    # Write results
-    echo "$cache_size,4,$assoc,$row" >> block_size.csv
+    ## Write results
+    echo "$cache_size,$block_size,$assoc,$row" >> $file
 
-    echo $hit_rate_prev
-    echo $(echo $sim | cut -d ' ' -f1)
-    # Update flag
+    ## Update flag
     flag=$(echo $hit_rate_prev '==' $(echo $sim | cut -d ' ' -f1) | bc -l)
-    echo $flag
+done
+
+## ---------------------------
+## Impact of Block Size
+## ---------------------------
+
+## Starting conditions
+cache_size=8192
+hit_rate_prev=0
+assoc=2
+block_size=4
+file="output/block_size_$var.csv"
+
+## File header
+echo "cache_size, block_size, associativity, inst_hit, data_hit" > $file
+
+## While
+while [ $block_size -lt 4096 ]
+do
+    ## Update hit_rate, cache_size, assoc, sim and row
+    sim=$(./sim -bs $block_size -a $assoc -wb -wa -ds $cache_size -is $cache_size  "./trazas/$trace" |
+                 grep hit | awk -F '(' '{print $2}' | sed -r 's/[^0-9.]//g')
+    hit_rate_prev=$(echo $sim | cut -d ' ' -f1)
+    row=$(echo $sim | sed 's/ /,/g')
+    block_size=$(($block_size*2))
+
+    ## Write results
+    echo "$cache_size,$block_size,$assoc,$row" >> $file
+
+    ## Update flag
+    flag=$(echo $hit_rate_prev '==' $(echo $sim | cut -d ' ' -f1) | bc -l)
+done
+
+## ---------------------------
+## Impact of Associativity
+## ---------------------------
+
+## Starting conditions
+cache_size=8192
+hit_rate_prev=0
+assoc=1
+block_size=128
+file="output/associativity_$var.csv"
+
+## File header
+echo "cache_size, block_size, associativity, inst_hit, data_hit" > $file
+
+## While
+while [ $assoc -lt 64 ]
+do
+    ## Update hit_rate, cache_size, assoc, sim and row
+    sim=$(./sim -bs $block_size -a $assoc -wb -wa -ds $cache_size -is $cache_size  "./trazas/$trace" |
+                 grep hit | awk -F '(' '{print $2}' | sed -r 's/[^0-9.]//g')
+    hit_rate_prev=$(echo $sim | cut -d ' ' -f1)
+    row=$(echo $sim | sed 's/ /,/g')
+    assoc=$(($assoc*2))
+
+    ## Write results
+    echo "$cache_size,$block_size,$assoc,$row" >> $file
+
+    ## Update flag
+    flag=$(echo $hit_rate_prev '==' $(echo $sim | cut -d ' ' -f1) | bc -l)
 done
