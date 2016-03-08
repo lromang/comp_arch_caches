5e2c7166a2635daaef4b6474a57d190662224d14
lromang <luis.roangarci@gmail.com>
2016-03-07 18:52:57 -0600

    all graphs

--- carry_sim.sh
+++ carry_sim.sh
@@ -104,3 +104,65 @@ do
     ## Update flag
     flag=$(echo $hit_rate_prev '==' $(echo $sim | cut -d ' ' -f1) | bc -l)
 done
+## ---------------------------
+## Memory Bandwidth (write)
+## ---------------------------
+## Starting conditions
+cache_size=8192 #16384
+hit_rate_prev=0
+assoc=2 #4
+block_size=128 #64
+file="output/bandWidth_write_$var.csv"
+
+
+## File header
+echo "cache_size, block_size, associativity, write_policy , demand_fetch, copies_back" > $file
+
+## for
+for  i in `seq 1`
+do
+    ## Write through
+    sim_through=$(../mike/sim -bs $block_size -a $assoc -wt -nw -ds $cache_size -is $cache_size  "./trazas/$trace" |
+                         grep -E "(fetch|back)" | awk -F ':' '{print $2}' | sed -r 's/[^0-9.]//g')
+    row_through=$(echo $sim_through | sed 's/ /,/g')
+
+    ## Write back
+    sim_back=$(../mike/sim -bs $block_size -a $assoc -wb -nw -ds $cache_size -is $cache_size  "./trazas/$trace" |
+                         grep -E "(fetch|back)" | awk -F ':' '{print $2}' | sed -r 's/[^0-9.]//g')
+    row_back=$(echo $sim_back | sed 's/ /,/g')
+
+    ## Write results
+    echo "$cache_size,$block_size,$assoc,wt,$row_through" >> $file
+    echo "$cache_size,$block_size,$assoc,wb,$row_back"    >> $file
+done
+
+## ---------------------------
+## Memory Bandwidth (allocate)
+## ---------------------------
+## Starting conditions
+cache_size=16384 #16384
+hit_rate_prev=0
+assoc=2 #4
+block_size=64 #64
+file="output/bandWidth_allocate_$var.csv"
+
+## File header
+echo "cache_size, block_size, associativity, allocate_policy ,demand_fetch, copies_back" > $file
+
+## for
+for  i in `seq 1`
+do
+    ## Write through
+    sim_allocate=$(../mike/sim -bs $block_size -a $assoc -wb -wa -ds $cache_size -is $cache_size  "./trazas/$trace" |
+                         grep -E "(fetch|back)" | awk -F ':' '{print $2}' | sed -r 's/[^0-9.]//g')
+    row_allocate=$(echo $sim_allocate | sed 's/ /,/g')
+
+    ## Write back
+    sim_non_allocate=$(../mike/sim -bs $block_size -a $assoc -wb -nw -ds $cache_size -is $cache_size  "./trazas/$trace" |
+                         grep -E "(fetch|back)" | awk -F ':' '{print $2}' | sed -r 's/[^0-9.]//g')
+    row_non_allocate=$(echo $sim_non_allocate | sed 's/ /,/g')
+
+    ## Write results
+    echo "$cache_size,$block_size,$assoc,wa,$row_allocate" >> $file
+    echo "$cache_size,$block_size,$assoc,nw,$row_non_allocate"    >> $file
+done
--- graphs.R
+++ graphs.R
@@ -38,8 +38,8 @@ for(i in 1:3){
 }
 
 ## Block size
-# i = 4, 5, 6
-for(i in 4:6){
+# i = 10, 11, 12
+for(i in 10:12){
     data_assoc <- gather(data[[i]][,-c(1,3)], cache_type, hit_rate, -block_size)
     png(paste0("./output/graphs/",names[i],".png"))
     ggplot(data = data_assoc, aes(x = block_size, y = hit_rate, col = cache_type)) +
@@ -59,8 +59,8 @@ for(i in 4:6){
 }
 
 ## Cache size
-# i = 7, 8, 9
-for(i in 7:9){
+# i = 13, 14, 15
+for(i in 13:15){
     data_assoc <- gather(data[[i]][,-c(2,3)], cache_type, hit_rate, -cache_size)
     png(paste0("./output/graphs/",names[i],".png"))
     ggplot(data = data_assoc, aes(x = cache_size, y = hit_rate, col = cache_type)) +
@@ -78,3 +78,51 @@ for(i in 7:9){
         xlab("Tamaño de cache") + ylab("Tasa hit")
     dev.off()
 }
+
+## Bandwidth
+
+## Write
+write    <- rbind(data[[7]], data[[8]], data[[9]])
+write$tot_traffic <- write[,5] + write[,6]
+data_assoc <- gather(write[,-c(1,2,3,5,6)], cache_type, tot_traffic, -write_policy)
+data_assoc$simul <- c(1,1,2,2,3,3)
+## PNG
+png(paste0("./output/graphs/write.png"))
+ggplot(data = data_assoc, aes(x = simul, y = tot_traffic, col = write_policy)) +
+    geom_point(size = 3) + geom_smooth(se = FALSE, span = .7, alpha=.3) +
+    theme(panel.background = element_blank(),
+          axis.title = element_text(colour = "#424242",
+                                    face = "bold"),
+          legend.text = element_text(colour = "#424242"),
+          legend.title = element_text(colour = "#424242",
+                                      face = "bold"),
+          axis.text = element_text(colour = "#424242")) +
+    scale_colour_manual(name   = "Política",
+                        labels = c("Wb", "Wt"),
+                        values = c("#1a237e", "#9e9e9e")) +
+    xlab("Simul") + ylab("Tráfico")
+dev.off()
+
+
+
+## allocate
+allocate <- rbind(data[[4]], data[[5]], data[[6]])
+allocate$tot_traffic <- allocate[,5] + allocate[,6]
+data_assoc <- gather(allocate[,-c(1,2,3,5,6)], cache_type, tot_traffic, -allocate_policy)
+data_assoc$simul <- c(1,1,2,2,3,3)
+## PNG
+png(paste0("./output/graphs/allocate.png"))
+ggplot(data = data_assoc, aes(x = simul, y = tot_traffic, col = allocate_policy)) +
+    geom_point(size = 3) + geom_smooth(se = FALSE, span = .7, alpha=.3) +
+    theme(panel.background = element_blank(),
+          axis.title = element_text(colour = "#424242",
+                                    face = "bold"),
+          legend.text = element_text(colour = "#424242"),
+          legend.title = element_text(colour = "#424242",
+                                      face = "bold"),
+          axis.text = element_text(colour = "#424242")) +
+    scale_colour_manual(name   = "Política",
+                        labels = c("Wb", "Wt"),
+                        values = c("#1a237e", "#9e9e9e")) +
+    xlab("Simul") + ylab("Tráfico")
+dev.off()
